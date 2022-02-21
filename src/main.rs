12ab8ea902bc641c42bbf1f58ae0cc359448bec0
Nathan Stocks <cleancut@github.com>
2022-02-21 12:54:02 -0700

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -91,7 +91,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
             }
             menu.draw(&mut curr_frame);
-            
+
             let _ = render_tx.send(curr_frame);
             thread::sleep(Duration::from_millis(1));
             continue;
